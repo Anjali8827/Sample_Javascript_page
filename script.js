a3b676a25a34d4f3a578b479cdbd0124633c9f61
Anjali8827 <pawaranjali248@gmail.com>
2024-11-10 22:27:04 +0530

    changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var istatus = document.querySelector("h5")
 var addFriend  = document.querySelector("#add")
-var removeFriend = document.querySelector("#Remove")
+var removeFriend = document.querySelector("#remove")
 
 addFriend.addEventListener("click",function(){
     istatus.innerHTML= "Friends"
